754b92e6e34a59f65f236235ace7af497a6c263c
padoleshital <shitalpadole7@gmail.com>
2025-02-09 14:36:41 +0530

    Concept of promise
    Signed-off-by: padoleshital <shitalpadole7@gmail.com>

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, interval, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit{
+ //subscribe
+  sub1 : Subscription | undefined;
+  sub2 : Subscription | undefined;
+  sub3 : Subscription | undefined;
+  //message
+  msg1:any;
+  msg2:any;
+  msg3:any;
+  constructor(){}
+
+  ngOnInit() {
+    const broadCastVideo = interval(1000)
+    //Ex - 01
+    this.sub1 = broadCastVideo.pipe(
+      map(data =>
+        'Video ' + data
+        // console.log('Video'+' ' + data)
+    )).subscribe(res =>{
+      this.msg1 = res
+      })
+    
+    setTimeout(() =>{
+      this.sub1?.unsubscribe();
+    },10000);
+
+
+
+    //Ex - 02
+  this.sub2 = broadCastVideo.pipe(
+    map(data => (data * 10))  
+  ).subscribe(res =>{
+    this.msg2 = res
+  });
+
+  setTimeout(() =>{
+    this.sub2?.unsubscribe();
+  },10000)
+
+
+  //Ex - 03
+  const members = from([
+    { id:1,age:30, name:'shital'},
+    { id:2,age:30, name:'sham'},
+    { id:3,age:30, name:'mohan'},
+    { id:4,age:30, name:'govind'},
+    { id:5,age:30, name:'basuri'},
+    { id:6,age:30, name:'kashi'},
+    { id:7,age:30, name:'shiv'},
+    { id:8,age:30, name:'shakti'},
+  ])
+
+  members.pipe(
+    map(data => data.name)
+  ).subscribe(res =>{
+    this.msg3 = res
+  })
+
+  
+  
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+//Ex - 03
+
 
 }
--- src/app/promises/promises.component.ts
+++ src/app/promises/promises.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-promises',
   templateUrl: './promises.component.html',
   styleUrls: ['./promises.component.css']
 })
-export class PromisesComponent {
+export class PromisesComponent implements OnInit {
+  constructor() { }
 
-}
+  DellAvailable() {
+    return setTimeout(() => {
+    return true
+    },3000)
+  }
+
+  HpAvailable()
+  {  
+    return setTimeout(() => {
+    return false;
+  },3000);
+  }
+
+  ngOnInit() {
+
+    //Basic example of Promise
+    
+    let buyLaptop = new Promise((resolve, reject) => {     
+         // resolve('Promise is resolved');
+          if(this.DellAvailable()){
+            resolve('Dell Laptop is available');
+          }
+          else if(this.HpAvailable()){
+            reject('Hp Laptop is available');          
+          }
+          else{
+            reject('Laptop is not available')
+          }
+        
+    })
+    buyLaptop.then(res =>{
+      console.log('then code =>', res);
+    })
+    buyLaptop.catch(res =>{
+      console.log('then code =>', res);
+    }) 
+
+  }
+
+  myFunction(){
+    console.log("Hello, I am from myFunction");
+  }
+   
+ 
+} 

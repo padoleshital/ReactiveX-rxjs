a0e6f4011df94de15e3c47ac01734561d2af4d1d
padoleshital <shitalpadole7@gmail.com>
2025-02-10 22:11:57 +0530

    Worked on timer and interval
    Signed-off-by: padoleshital <shitalpadole7@gmail.com>

--- src/app/observables/interval/interval.component.ts
+++ src/app/observables/interval/interval.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Subscription, timer } from 'rxjs';
+import { DesignUtilityService } from 'src/app/appServices/design-utility.service';
 
 @Component({
   selector: 'app-interval',
@@ -6,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interval.component.css']
 })
 export class IntervalComponent implements OnInit {
-   constructor() { }
+  obsMesg:any;
+  videoSubscription:Subscription = new Subscription();
+   constructor(private designUtility:DesignUtilityService) { }
     ngOnInit() {
+      // let broadCastVideos = interval(1000); //interval is a function that emits a sequence of numbers in a specified interval of time.
+
+      let broadCastVideos = timer(1000,6000);
+
+      broadCastVideos.subscribe(res =>{
+        console.log(res);
+        this.obsMesg = 'Video ' + res;
+        this.designUtility.elAppend(this.obsMesg,'elContainer');
+        this.designUtility.elAppend(this.obsMesg,'elContainer1');
+        this.designUtility.elAppend(this.obsMesg,'elContainer2');
+        if(res>=5){
+          this.videoSubscription.unsubscribe();
+        }
+
+      })
+
     }
+
+
 }
